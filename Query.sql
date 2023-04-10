75b4264a35ce000a5455d5c844bc49f804209247
kaizermm <121756502+kaizermm@users.noreply.github.com>
2023-04-10 10:39:44 -0400

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -8,6 +8,16 @@ FROM [data_analysis_stack_exchange_movies].[dbo].[posts]
 GROUP BY YEAR(CreationDate)
 -- Q2.How many votes were made in each day of the week (Sunday, Monday, Tuesday, etc.) ?*/
 /* --------- SOLUTION --------- */
-
+SELECT 
+COUNT(*) AS 'Number of Posts', 
+FORMATMESSAGE('%s',DATENAME(WEEKDAY, CAST(CreationDate AS DATE))) AS 'Day of the Week'
+FROM [data_analysis_stack_exchange_movies].[dbo].[Votes]
+GROUP BY DATEPART(WEEKDAY, CAST(CreationDate AS DATE)), 
+FORMATMESSAGE('%s',DATENAME(WEEKDAY, CAST(CreationDate AS DATE)))
 -- Q3.List all comments created on September 19th, 2012
+/* --------- SOLUTION --------- */
+SELECT *
+FROM [data_analysis_stack_exchange_movies].[dbo].[Comments]
+where CONVERT(DATE, creationDate) = '2012-12-19'
+
 
