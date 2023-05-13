8ea02231f9ccaeac15e6af6cd888f4d969cbb78d
Kevin(Wan Quyen) Huang <121756502+kaizermm@users.noreply.github.com>
2023-05-13 13:22:00 -0400

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -18,7 +18,8 @@ company_name
 order by
 trips_amount
 desc;
-/*This SQL query is used to retrieve the number of trips made by each taxi company during a specific time period. 
+/*Explaination of the query:
+This SQL query is used to retrieve the number of trips made by each taxi company during a specific time period. 
 The query uses the "cabs" and "trips" tables, joining them based on the "cab_id" column. 
 The WHERE clause is used to filter the trips that started between November 15th and November 16th, 2017. 
 The GROUP BY clause groups the results by company name, and the COUNT function is used to count the number of trips made by each company. 
@@ -48,20 +49,48 @@ cabs.company_name LIKE '%Blue%' and
 trips.start_ts >= '2017-11-01' AND trips.start_ts < '2017-11-08'
 group by
 company_name;
-/*This SQL query retrieves the number of trips made by the Yellow and Blue taxi companies between November 1 and November 7, 2017. 
+/*Explaination of the query:
+This SQL query retrieves the number of trips made by the Yellow and Blue taxi companies between November 1 and November 7, 2017. 
 It uses a union to combine the results of two queries. 
 The first query counts the number of trips made by Yellow taxis during the specified period, while the second query counts the number of trips made by Blue taxis during the same period. 
 Both queries use a join between the cabs and trips tables to match trips to the correct taxi company based on cab_id. 
 The LIKE operator is used in both queries to search for company names containing the specified substrings ('Yellow' and 'Blue', respectively). 
 The result set includes two columns: company_name (the name of the taxi company) and trips_amount (the number of trips made by that company during the specified period).
 */
--- Q3.List all comments created on September 19th, 2012
+-- Q3.For November 1-7, 2017, the most popular taxi companies were Flash Cab and Taxi Affiliation Services. 
+Find the number of rides for these two companies and name the resulting variable trips_amount. 
+Join the rides for all other companies in the group "Other." Group the data by taxi company names. 
+Name the field with taxi company names company. Sort the result in descending order by trips_amount.
 /* --------- SOLUTION --------- */
-SELECT *
-FROM [data_analysis_stack_exchange_movies].[dbo].[Comments]
-where CONVERT(DATE, creationDate) = '2012-12-19'
--- Advanced Analysis QUESTIONS --
-Q1.Display the number of votes for each post title*/
+SELECT 
+    CASE 
+        WHEN cabs.company_name = 'Flash Cab' THEN 'Flash Cab' 
+        WHEN cabs.company_name = 'Taxi Affiliation Services' THEN 'Taxi Affiliation Services' 
+        ELSE 'Other' 
+    END AS company, 
+    COUNT(trips.trip_id) AS trips_amount
+FROM 
+    cabs 
+    INNER JOIN trips ON trips.cab_id = cabs.cab_id 
+WHERE 
+    CAST(trips.start_ts AS DATE) BETWEEN '2017-11-01' AND '2017-11-07'
+GROUP BY 
+    CASE 
+        WHEN cabs.company_name = 'Flash Cab' THEN 'Flash Cab' 
+        WHEN cabs.company_name = 'Taxi Affiliation Services' THEN 'Taxi Affiliation Services' 
+        ELSE 'Other' 
+    END
+ORDER BY 
+    trips_amount DESC;
+/*Explaination of the query:
+this SQL query retrieves the number of taxi rides by company for a specified date range. 
+The result groups the rides by company name and counts the number of trips for each group. 
+The query uses a conditional statement in the SELECT clause to group the companies into three categories: "Flash Cab," "Taxi Affiliation Services," and "Other." 
+It joins the "cabs" and "trips" tables on the "cab_id" field to obtain information about the taxi rides.
+The WHERE clause filters the results to include only trips that occurred between November 1st, 2017 and November 7th, 2017. 
+Finally, the query orders the results in descending order by the number of trips for each company.
+*/
+Q4.Display the number of votes for each post title*
 SELECT pst.Title,count(*) number_of_votes
 FROM [data_analysis_stack_exchange_movies].[dbo].[Votes] as vt
 join
