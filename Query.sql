c36658f5458b6d6df550539161f754150bfcfce7
Kevin(Wan Quyen) Huang <121756502+kaizermm@users.noreply.github.com>
2023-10-21 14:43:58 -0400

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -1,6 +1,7 @@
 /* AUTHOR   - KEVIN HUANG(WAN QUYEN HUANG)
  Tool Used  - PostgreSQL
- Created on - May'2023*/
+ Since the data for the project is located within the TripleTen interface, most of the SQL will be executed on the TripleTen website interface.
+ Created on - May-2023*/
 --------------------------------------------------------------------------
 -- Analysis QUESTIONS --
 -------------------------------------------------------------------------
@@ -24,10 +25,44 @@ Finally, the results are sorted in descending order based on the number of trips
 
 /* Q2. Could you please generate the SQL query that retrieves the pickup location names and the total number of trips taken to each pickup location,
 while also joining the 'cabs,' 'trips,' and 'neighborhoods' tables to associate the data correctly and sorting the results in descending order of trip count? */
+/* --------- SOLUTION --------- */
+select 
+    neighborhoods.name as pickup_location,count(trips.trip_id) as n_trip
+from cabs
+join trips on cabs.cab_id=trips.cab_id
+join  neighborhoods on neighborhoods.neighborhood_id=trips.pickup_location_id
+group by
+drop_location
+order by
+n_trip
+desc;
+/*Explanation of the query:
+This SQL query retrieves data on pickup locations from the 'neighborhoods' table and calculates the total number of trips originating from each neighborhood, which is then assigned to the 'pickup_location' alias. 
+The data is merged with information from the 'cabs' and 'trips' tables, connecting via cab IDs and pickup location IDs. The results are grouped by 'drop_location' and ordered in descending order by the number of trips. 
+In essence, it provides insights into which neighborhoods serve as frequent pickup points for rides.
+
+/* Q3.Could you please create an SQL query to retrieve the drop-off locations with the highest number of trips, using the "neighborhoods" dataset's "name" column, joined with the "cabs" and "trips" tables? 
+The query should calculate the count of trips for each drop-off location and order the results in descending order of trip count.
+/* --------- SOLUTION --------- */
+select 
+    neighborhoods.name as drop_location,count(trips.trip_id) as n_trip
+from cabs
+join trips on cabs.cab_id=trips.cab_id
+join  neighborhoods on neighborhoods.neighborhood_id=trips.dropoff_location_id
+group by
+drop_location
+order by
+n_trip
+desc;
+
+/*Explanation of the query:
+This SQL query combines data from multiple tables, including "cabs," "trips," and "neighborhoods". 
+It retrieves the name of the drop-off locations from the "neighborhoods" table and counts the number of trips to each location. 
+The results are grouped by drop-off location and ordered in descending order based on the count of trips (n_trip). 
+This query provides valuable insights into the neighborhoods with the highest number of trips, helping to identify popular drop-off locations for cab services.
 
 
- */
- /* Q6.Retrieve from the trips table all the rides that started in the Loop (pickup_location_id: 50) on a Saturday and ended at O'Hare 
+/* Q4.Retrieve from the trips table all the rides that started in the Loop (pickup_location_id: 50) on a Saturday and ended at O'Hare 
  (dropoff_location_id: 63). Get the weather conditions for each ride. Use the method you applied in the previous task. Also, retrieve 
  the duration of each ride. Ignore rides for which data on weather conditions is not available.*/
 The table columns should be in the following order:
